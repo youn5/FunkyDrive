1e808ccc176cb3a54bd07d0f14a4ba72aa2bc0a6
Youn5 <etud8@yahoo.fr>
2024-03-05 11:34:49 +0100

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.funkyDrive.funkydrive"
---
+++ src/main/kotlin/com/funkyDrive/plugins/HTTP.kt
@@ -0,0 +1,26 @@
+package com.funkyDrive.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.routing.*
+
+fun Application.configureHTTP() {
+    routing {
+        swaggerUI(path = "openapi")
+    }
+    routing {
+        openAPI(path = "openapi")
+    }
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader("MyCustomHeader")
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+}
